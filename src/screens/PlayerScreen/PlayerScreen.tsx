3841c9b71db7bef375ab40d2285d718bc6dd5ef2
lobitz <lobitz@sipgate.de>
2019-09-06 12:10:15 +0200

    wip: add get teamname component

--- src/screens/PlayerScreen/PlayerScreen.tsx
+++ src/screens/PlayerScreen/PlayerScreen.tsx
@@ -6,12 +6,30 @@ interface IProps {
 	path: string;
 }
 
-const PlayerButton: React.FC = () => <button className={styles.playerButton}>ANSWER</button>;
+const PlayerButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
+	<button className={styles.playerButton} onClick={onClick}>
+		ANSWER
+	</button>
+);
 
+const GetTeamInput: React.FC<{ onClick: () => void }> = ({ onClick }) => (
+	<div className={styles.teamInputWrapper}>
+		<input className={styles.teamInput} placeholder="Gib einen Teamnamen ein" type="text" />
+
+		<button className={styles.teamInputButton} onClick={onClick}>
+			Absenden
+		</button>
+	</div>
+);
 export const PlayerScreen: React.FC<IProps> = ({ userId }) => {
+	const handleDislike = () => {
+		return () => {
+			console.log("dislike");
+		};
+	};
 	return (
 		<div className={styles.root}>
-			<PlayerButton />
+			<GetTeamInput onClick={handleDislike} />
 		</div>
 	);
 };
